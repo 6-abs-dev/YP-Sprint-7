bce7438d292bc992c4c7f0ff2640f78018865935
Maksim Ionov <maksim.ionov@t-systems.com>
2024-11-24 12:01:27 +0100

    sprint 7 tasks

--- Exc4/1_create_users.sh
+++ Exc4/1_create_users.sh
@@ -2,7 +2,7 @@
 
 kubectl create serviceaccount reader -n default
 kubectl create serviceaccount developer -n default
-kubectl create serviceaccount cccccccc -n default
+kubectl create serviceaccount secrets-manager -n default
 
 kubectl create token reader -n default
 kubectl create token developer -n default
